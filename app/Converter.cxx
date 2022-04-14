3a993c726eaf54fd69e93751e472e22a74b6483d
Sergey Suvorov <suvorovsb@gmail.com>
2022-04-14 14:08:59 +0200

    Update CLI

--- app/Converter.cxx
+++ app/Converter.cxx
@@ -3,115 +3,111 @@
 
 #include <iostream>
 
+#include "CmdLineParser.h"
+
 #include "TString.h"
 
-/*******************************************************************************
-help() to display usage
-*******************************************************************************/
-void help()
-{
-  printf("fdecoder <options>\n");
-  printf("   -h                   : print usage\n");
-  printf("   -i <input_file>      : input file name with a path\n");
-  printf("   -o <output_path>     : output files path (default='.')\n");
-  printf("   -p <Value>           : number of pre-samples below threshold in zero-suppressed mode\n");
-  printf("   -n <Value>           : spesify the number of events");
-  printf("   -v <level>           : verbose\n");
-  printf("   -t                   : test converter workability - run iver 30 first events\n");
-  printf("   -s <tracker_file>    : embed info from the external tracker");
-  printf("   -f <0xFlags>         : flags to determine printed items\n");
-  exit(1);
-}
-
-// ******************************************************************************
-// parse_cmd_args() to parse command line arguments
-// ******************************************************************************
-int parse_cmd_args(int argc, char **argv, Param *p) {
-  for (;;) {
-    int c = getopt(argc, argv, "i:o:v:p:n:hts:");
-    if (c < 0) break;
-    switch (c) {
-      case 'i' : p->inp_file                = optarg;       break;
-      case 'o' : p->out_path                = optarg;       break;
-      case 'v' : p->verbose                 = atoi(optarg); break;
-      case 't' : p->test                    = true;         break;
-      case 'f' : p->vflag                   = atoi(optarg); break;
-      case 'p' : p->sample_index_offset_zs  = atoi(optarg); break;
-      case 's' : p->tracker_file            = optarg;       break;
-      case 'n' : p->nevents                 = atoi(optarg); break;
-      default : help();
-    }
-  }
-  if (argc == 1)
-    help();
-
-  return 0;
-}
-
 int main(int argc, char **argv) {
-   Param param;
+//    Param param;
 
-   if (parse_cmd_args(argc, argv, &param) < 0)
-    return (-1);
+    // inti CLI module
+    CmdLineParser clParser;
+    clParser.setIsUnixGnuMode(true);
+    clParser.setIsFascist((true));
 
-   // define input file and the proper interface to read it
-   TString file_name = TString(param.inp_file);
-   std::shared_ptr<InterfaceBase> interface = InterfaceFactory::get(file_name);
+    // define CLI
+    clParser.addOption("input_file", {"-i", "--input"}, "Input file name");
+    clParser.addOption("output_path", {"-o", "--output"}, "Output path");
+    clParser.addOption("verbose", {"-v", "--verbose"}, "Verbosity level");
+    clParser.addOption("tracker", {"-s", "--silicon"}, "Add silicon tracker info");
+    clParser.addOption("nEventsFile", {"-n", "--nEventsFile"}, "Number of events to process");
 
-   if (!interface->Initialise(file_name, param.verbose)) {
-      std::cerr << "Interface initialisation fails. Exit" << std::endl;
-      exit(1);
-   }
+    clParser.addOption("array", {"--array"}, "Convert to 3D array");
+    clParser.addOption("card", {"-c", "--card"}, "Specify the particular card that will be converted.");
 
-   // Whether to read silicon tracker stuff
-   auto tracker = std::make_shared<InterfaceTracker>();
-   TString tracker_name = TString(param.tracker_file);
-   auto read_tracker = tracker->Initialise(tracker_name, param.verbose);
+    clParser.addTriggerOption("help", {"-h", "--help"}, "Print usage");
 
-   // extract the file name from the input
-   TString out_file = OutputBase::getFileName(param.out_path, param.inp_file);
+    // do parsing
+    clParser.parseCmdLine(argc, argv);
 
-   // Select the output format
-   // TODO add CLI for this choice
-   std::shared_ptr<OutputBase> output = std::make_shared<OutputTRawEvent>();
-   output->Initialise(out_file, read_tracker);
-
-   // define the output events number
-   long int Nevents;
-   Nevents = interface->Scan(-1, true, tmp);
-
-   if (read_tracker) {
-     long int N_tracker = tracker->Scan(-1, true, tmp);
-     std::cout << N_tracker << " events in the tracker file" << std::endl;
-     // overflow by 1 is allowed by trigger design
-     if (N_tracker - 1 > Nevents)
-       std::cerr << "Number of events in tracker is larger then in TPC" << std::endl;
-   }
-  if (param.nevents > 0) {
-    Nevents = std::min(Nevents, (long int)param.nevents);
-  }
-  if (!param.verbose)
-    std::cout << "Doing conversion" << "\n[                     ]\r[" << std::flush;
-
-  for (long int i = 0; i < Nevents; ++i) {
-    if (param.verbose)
-      std::cout << "Working on " << i << std::endl;
-    else if (Nevents/20 > 0) {
-      if (i % (Nevents / 20) == 0)
-      std::cout << "#" << std::flush;
+    if (clParser.isOptionTriggered("help")) {
+        std::cout << clParser.getConfigSummary();
+        exit(0);
     }
-    output->AddEvent(interface->GetEvent(i));
+
+    // assign results
+    auto fileName = clParser.getOptionVal<std::string>("input_file", "", 0);
+    auto outPath = clParser.getOptionVal<std::string>("output_path", "", 0);
+    auto trackerName = clParser.getOptionVal<std::string>("tracker", "", 0);
+
+    auto nEventsRead = clParser.getOptionVal<uint64_t>("nEventsFile", 0, 0);
+    auto verbose = clParser.getOptionVal<int>("verbose", 1, 0);
+
+    bool useArray = clParser.isOptionTriggered("array");
+    auto card = clParser.getOptionVal<int>("card", 0, 0);
+
+    // define the proper interface to read it
+    std::shared_ptr<InterfaceBase> interface = InterfaceFactory::get(fileName);
+    if (!interface->Initialise(fileName, verbose)) {
+        std::cerr << "Interface initialisation fails. Exit" << std::endl;
+        exit(1);
+    }
+
+    // Whether to read silicon tracker stuff
+    auto tracker = std::make_shared<InterfaceTracker>();
+    auto read_tracker = tracker->Initialise(trackerName, verbose);
+
+    // extract the file name from the input
+    TString out_file = OutputBase::getFileName(outPath, fileName);
+
+    // Select the output format
+    // TODO add CLI for this choice
+    std::shared_ptr<OutputBase> output;
+    if (useArray) {
+        output = std::make_shared<OutputArray>();
+        output->SetCard(card);
+    } else {
+        output = std::make_shared<OutputTRawEvent>();
+    }
+    output->Initialise(out_file, read_tracker);
+
+    // define the output events number
+    uint64_t nEventsFile;
+    nEventsFile = interface->Scan(-1, true, tmp);
 
     if (read_tracker) {
-        std::vector<float> tracker_data;
-        tracker->GetEvent(i, tracker_data);
-        output->AddTrackerEvent(tracker_data);
+        uint64_t N_tracker = tracker->Scan(-1, true, tmp);
+        std::cout << N_tracker << " events in the tracker file" << std::endl;
+        // overflow by 1 is allowed by trigger design
+        if (N_tracker - 1 > nEventsFile)
+            std::cerr << "Number of events in tracker is larger then in TPC" << std::endl;
     }
-    output->Fill();
-  } // loop over events
+    if (nEventsRead > 0) {
+        nEventsFile = std::min(nEventsFile, nEventsRead);
+    }
+    if (verbose == 1)
+        std::cout << "Doing conversion" << "\n[                     ]\r[" << std::flush;
 
-  output->Finilise();
-  std::cout << "\nConversion done" << std::endl;
+    for (long int i = 0; i < nEventsFile; ++i) {
+        if (verbose > 1)
+            std::cout << "Working on " << i << std::endl;
+        else if (verbose == 1 && nEventsFile / 20 > 0) {
+            if (i % (nEventsFile / 20) == 0)
+                std::cout << "#" << std::flush;
+        }
+        output->AddEvent(interface->GetEvent(i));
 
-  return 0;
+        if (read_tracker) {
+            std::vector<float> tracker_data;
+            tracker->GetEvent(i, tracker_data);
+            output->AddTrackerEvent(tracker_data);
+        }
+        output->Fill();
+    } // loop over events
+
+    output->Finilise();
+    if (verbose > 0)
+        std::cout << "\nConversion done" << std::endl;
+
+    return 0;
 }
